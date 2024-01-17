44f755b63dd060b45e615033e5a5d55bdf5c9af2
Jake Gauthier <jake@net10.net>
2024-01-16 16:45:24 -1000

    index.html

--- get_freq.sh
+++ get_freq.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "<table>" > freq.html
+echo "<table>" > index.html
 
 for i in {1..264}
 do
     echo "Page ${i}"
-    curl "https://www.scannerfrequencies.com/radio/hawaii/honolulu/page/${i}" | sed -n '/<tr class="odd_even">/,/<\/tr>/p' >> freq.html
+    curl "https://www.scannerfrequencies.com/radio/hawaii/honolulu/page/${i}" | sed -n '/<tr class="odd_even">/,/<\/tr>/p' >> index.html
 done
 
 
-echo "</table>" >> freq.html
+echo "</table>" >> index.html
